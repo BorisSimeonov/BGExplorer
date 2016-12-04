0285dbe20a5cdfa001282b9d8a15c709aea1fbe6
twistbobg@gmail.com <twistbobg@gmail.com>
2016-12-04 21:46:16 +0200

    - Added refresh and post buttons and styling in comments section

--- src/Views/ArticleCommentView/ArticleCommentView.js
+++ src/Views/ArticleCommentView/ArticleCommentView.js
@@ -12,23 +12,33 @@ export default class ArticleCommentView extends React.Component {
         if (commentsArray && commentsArray.length) {
             commentsArray = commentsArray.map(commentObj =>
                 (<div key={commentObj._id} className="article-feedback-post">
-                    <h3><span className="article-comment-owner-name">{commentObj.comment.from}</span>
-                        <span
-                            className="article-comment-delete">{
-                            (commentObj._acl.creator === appStore.userData.userId) ?
-                                <button>Delete</button> : null
-                        }</span>
-                    </h3>
-                    <div className="article-comment-text">{commentObj.comment.text}</div>
-                </div>)
+                        <h3><span className="article-comment-owner-name">{commentObj.comment.from}</span>
+                            <span
+                                className="article-comment-delete">{
+                                (commentObj._acl.creator === appStore.userData.userId) ?
+                                    <button>Delete</button> : null
+                            }</span>
+                        </h3>
+                        <div className="article-comment-text">{commentObj.comment.text}</div>
+
+                    </div>
+                )
             );
         }
 
         if (sessionStorage.getItem('authToken')) {
             return (
-                <div className="article-feedback-holder">
+                <form className="article-feedback-holder">
                     {commentsArray}
-                </div>
+                    <div className="article-comment-composer">
+                        <div>
+                            <h3>Add new comment:</h3>
+                            <textarea />
+                        </div>
+                        <input className="article-comment-button" type="button" value={"Post Comment"}/>
+                        <input className="article-comment-button" type="button" value={"Refresh Comments"}/>
+                    </div>
+                </form>
             )
         } else {
             return (
